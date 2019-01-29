6e805754c5493a84290fd801d9e70079d1182d6d
P-Hugo <ph.meillot@gmail.com>
2019-01-29 16:26:00 +0100

    remove slash

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 
+const PATH = path.join(__dirname,'./dist/ailyeWebSite/');
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname ,'dist/ailyeWebsite')));
+app.use(express.static(path.join(__dirname, './dist/ailyeWebsite')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/ailyeWebsite/index.html'));
+  res.sendFile(path.join(__dirname, './dist/ailyeWebsite', 'index.html'));
 });
+// /home/ailye/Documents/ailyeWebSite/dist/ailyeWebSite
+
+function showFiles(file) {
+  console.log(file)
+  fs.readdir(file, (err, files) => {
+    if (!err) {
+      files.forEach(e => {
+        console.log(e);
+        if (fs.lstatSync(e).isDirectory()) {
+          showFiles(e);
+        }
+      });
+    }
+  })
+}
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => console.log('Serveur up'));
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Serveur up');
+  // showFiles(PATH)
+});

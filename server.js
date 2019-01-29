1c9c95b8156fee88eb8e88fa0b6df7d0c8e79b8a
P-Hugo <ph.meillot@gmail.com>
2019-01-29 11:33:46 +0100

    edit path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/ailyeWebsite'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join('./dist/ailyeWebsite/index.html'));
+res.sendFile(path.join(__dirname, '/dist/ailyeWebsite/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
